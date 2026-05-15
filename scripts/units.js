3f2dc1b1e4bf1925484c955b5812feae97bce343
LowGamik <lowgamik@gmail.com>
2026-05-15 12:01:09 +0200

    made progress on Centurion Mk. I

--- scripts/units.js
+++ scripts/units.js
@@ -1,6 +1,12 @@
-const centurion = extend(TankUnitType,"centurion",{
+const centurion = extend(TankUnitType,"centurion-mk-i",{
+    localizedName: "Centurion Mk. I",
     health: 800,
     armor: 20,
-    targetAir: false
+    targetAir: false,
+    speed: 0.5,
+    range: 120,
+    rotateSpeed: 2,
+    itemCapacity: 0,
+    hitSize: 20
 });
 centurion.constructor = () => extend(TankUnit,{});
